1e1c203be49e4727b323573c803104184435b259
qkr03210 <qkr03210@gmail.com>
2021-04-07 11:33:10 +0900

    예약 조회 추가

--- MyProject/src/MyInfo.java
+++ MyProject/src/MyInfo.java
@@ -20,14 +20,17 @@ import java.awt.event.MouseEvent;
 public class MyInfo extends JPanel{
 //	String id;
 	private JTable table = new JTable();
+	private JTable reserveTable=new JTable();
 	JScrollPane scrollPane;
+	JScrollPane scrollPane2;
 	
-	   //시간용
-	   SimpleDateFormat format1 = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
+	//시간용
+	SimpleDateFormat format1 = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
 	
 	Connection conn = null;
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
+	
 	
 	public MyInfo(){
 		setSize(790, 648);
@@ -38,10 +41,16 @@ public class MyInfo extends JPanel{
 		
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
-				{null, null, null, null, null},
 			},
 			new String[] {
 				"\uB300\uC5EC\uBC88\uD638", "\uB300\uC5EC\uC77C", "\uBC18\uB0A9\uC77C", "\uBC18\uB0A9\uC608\uC815\uC77C", "\uB3C4\uC11C\uBA85"
+			}
+		));
+		reserveTable.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"\uB3C4\uC11C \uBC88\uD638", "\uB3C4\uC11C\uBA85", "\uC800\uC790"
 			}
 		));
 		table.setBounds(41, 315, 1, 1);
@@ -55,7 +64,7 @@ public class MyInfo extends JPanel{
 //		add(selectedTable);
 		
 		scrollPane = new JScrollPane(table);
-		
+		scrollPane2 = new JScrollPane(reserveTable);
 
 		
 		scrollPane.setBounds(12, 57, 622, 405);
@@ -88,12 +97,25 @@ public class MyInfo extends JPanel{
 				nowRental();
 			}
 		});
+		
+		
+		scrollPane2.setBounds(12, 57, 622, 405);
+		add(scrollPane2);
+		scrollPane2.setVisible(false);
+		
+		
+		reserveTable.getColumnModel().getColumn(0).setPreferredWidth(150);
+		reserveTable.getColumnModel().getColumn(1).setPreferredWidth(389);
+		reserveTable.setBounds(0, 0, 1, 1);
+//		add(reserveTable);
 		btn_return.setBounds(657, 54, 97, 23);
 		add(btn_return);
 		
 		JButton btnNewButton = new JButton("전체 대여 내역");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				scrollPane.setVisible(true);
+				scrollPane2.setVisible(false);
 				AllRental();
 			}
 		});
@@ -104,6 +126,17 @@ public class MyInfo extends JPanel{
 		
 		btnNewButton_1.setBounds(149, 24, 125, 23);
 		add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("예약 내역");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				scrollPane.setVisible(false);
+				scrollPane2.setVisible(true);
+				myReserv();
+			}
+		});
+		btnNewButton_2.setBounds(286, 24, 125, 23);
+		add(btnNewButton_2);
 
 		table.addMouseListener(new MouseAdapter() {
 			@Override
@@ -118,6 +151,8 @@ public class MyInfo extends JPanel{
 
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				scrollPane.setVisible(true);
+				scrollPane2.setVisible(false);
 				nowRental();
 			}
 		});
@@ -181,6 +216,39 @@ public class MyInfo extends JPanel{
 				str[4]=rs.getString(5);
 				
 				model = (DefaultTableModel) table.getModel();
+				model.addRow(str);
+			}
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)	rs.close();
+				if (pstmt != null) pstmt.close();
+				if (conn != null) conn.close();
+			} catch (Exception closee) {
+			}
+		}
+	}
+	//21-04-07 박상준 작업중
+	public void myReserv(){
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.0.77:1521:xe", "AI", "1234");
+			pstmt = conn.prepareStatement("select r.ren_bid,bb.title,bb.author from lib_rental r, lib_books bb  where r.ren_bid=bb.bid and r.sun_id='"+MyProject.UserId+"'");
+			rs=pstmt.executeQuery();
+			
+			String str[] = new String[5];
+			DefaultTableModel model = (DefaultTableModel) reserveTable.getModel();
+			model.setNumRows(0);
+			while(rs.next())
+			{
+				str[0]=rs.getString(1);
+				str[1]=rs.getString(2);
+				str[2]=rs.getString(3);
+
+				
+				model = (DefaultTableModel) reserveTable.getModel();
 				model.addRow(str);
 			}
 		} catch (Exception e2) {
